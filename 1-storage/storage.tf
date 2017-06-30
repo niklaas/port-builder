c9dc34b2ea1f523b16a705f7e28a49549833ce09
Niklaas Baudet von Gersdorff <stdin@niklaas.eu>
2017-06-30 18:07:52 +0200

    Adds S3 bucket for storing built packages
    Closes #4

--- 1-storage/storage.tf
+++ 1-storage/storage.tf
@@ -1,18 +1,3 @@
-variable "aws_access_key" { }
-variable "aws_secret_key" { }
-variable "aws_region"     { }
-variable "ebs_size"       { }
-
-provider "aws" {
-    region     = "${var.aws_region}"
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-}
-
-data "aws_availability_zones" "available" {
-    state = "available"
-}
-
 resource "aws_ebs_volume" "poudriere" {
     availability_zone = "${data.aws_availability_zones.available.names[0]}"
     size              = "${var.ebs_size}"
@@ -22,6 +7,9 @@ output "ebs_id" {
     value = "${aws_ebs_volume.poudriere.id}"
 }
 
-output "availability_zone" {
-    value = "${aws_ebs_volume.poudriere.availability_zone}"
+resource "aws_s3_bucket" "packages" {
+    bucket = "${var.s3_bucket_name}"
+    acl    = "public-read"
+
+    website { }
 }
